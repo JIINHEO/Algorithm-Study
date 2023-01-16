4e16cfc73c30dc49236fa6a64f6c30ab50dec3c2
JIINHEO <gjwldls123@gmail.com>
2023-01-16 23:57:51 +0900

    Lv0. 문자열 계산하기

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -241,3 +241,30 @@ func solution_en_to_num(_ numbers:String) -> Int64 {
 print(solution_en_to_num("onetwothreefourfivesixseveneightnine"))
 print(solution_en_to_num("onefourzerosixseven"))
 print("=================================================")
+
+/*
+ [ 문자열 계산하기 ]
+
+ my_string은 "3 + 5"처럼 문자열로 된 수식입니다. 문자열 my_string이 매개변수로 주어질 때, 수식을 계산한 값을 return 하는 solution 함수를 완성해주세요.
+
+ 입출력 예
+ my_string    result
+ "3 + 4"    7
+ */
+
+func solution_cal_string(_ my_string:String) -> Int {
+    var arr = my_string.split(separator: " ")
+    var result = Int(arr[0])!
+    for i in arr.enumerated() {
+        if i.element == "+" {
+            result += Int(arr[i.offset + 1])!
+        }
+        if i.element == "-" {
+            result -= Int(arr[i.offset + 1])!
+        }
+    }
+    return result
+}
+
+print(solution_cal_string("3 + 4"))
+print("=================================================")
