21718453e14008b9dfae160422c9ea2675e4fe6a
JIINHEO <gjwldls123@gmail.com>
2023-01-19 00:52:30 +0900

    Lv0. 캐릭터의 좌표

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -367,3 +367,54 @@ func solution_rec_area(_ dots:[[Int]]) -> Int {
 }
 
 print(solution_rec_area([[1, 1], [2, 1], [2, 2], [1, 2]] ))
+print("=================================================")
+
+/*
+ [ 캐릭터의 좌표 ]
+ 
+ 머쓱이는 RPG게임을 하고 있습니다. 게임에는 up, down, left, right 방향키가 있으며 각 키를 누르면 위, 아래, 왼쪽, 오른쪽으로 한 칸씩 이동합니다. 예를 들어 [0,0]에서 up을 누른다면 캐릭터의 좌표는 [0, 1], down을 누른다면 [0, -1], left를 누른다면 [-1, 0], right를 누른다면 [1, 0]입니다. 머쓱이가 입력한 방향키의 배열 keyinput와 맵의 크기 board이 매개변수로 주어집니다. 캐릭터는 항상 [0,0]에서 시작할 때 키 입력이 모두 끝난 뒤에 캐릭터의 좌표 [x, y]를 return하도록 solution 함수를 완성해주세요.
+
+ [0, 0]은 board의 정 중앙에 위치합니다. 예를 들어 board의 가로 크기가 9라면 캐릭터는 왼쪽으로 최대 [-4, 0]까지 오른쪽으로 최대 [4, 0]까지 이동할 수 있습니다.
+ 
+ 입출력 예
+ keyinput    board    result
+ ["left", "right", "up", "right", "right"]    [11, 11]    [2, 1]
+ ["down", "down", "down", "down", "down"]    [7, 9]    [0, -4]
+ https://school.programmers.co.kr/learn/courses/30/lessons/120861
+ */
+
+func solution_cha_coordinate(_ keyinput:[String], _ board:[Int]) -> [Int] {
+    var x = 0
+    var y = 0
+    
+    let boardX = board[0] / 2
+    let boardY = board[1] / 2
+    
+    for move in keyinput {
+        switch move {
+        case "left":
+            if x > -boardX {
+                x -= 1
+            }
+        case "right":
+            if x < boardX {
+                x += 1
+            }
+        case "up":
+            if y < boardY {
+                y += 1
+            }
+        case "down":
+            if y > -boardY {
+                y -= 1
+            }
+        default:
+            print("")
+        }
+    }
+    return [x, y]
+}
+
+print(solution_cha_coordinate( ["left", "right", "up", "right", "right"], [11, 11]))
+print(solution_cha_coordinate( ["down", "down", "down", "down", "down"], [7, 9]))
+print("=================================================")
