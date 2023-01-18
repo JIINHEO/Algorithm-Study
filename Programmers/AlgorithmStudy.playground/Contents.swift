f934d7049207d972ea8bf83a63125d8fa15ddf22
JIINHEO <gjwldls123@gmail.com>
2023-01-19 00:16:43 +0900

    Lv.0 잘라서 배열로 저장하기

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -303,3 +303,36 @@ func solution_OX_quiz(_ quiz:[String]) -> [String] {
 
 print(solution_OX_quiz(["3 - 4 = -3", "5 + 6 = 11"]))
 print("=================================================")
+
+/*
+ [ 잘라서 배열로 저장하기 ]
+
+ 문자열 my_str과 n이 매개변수로 주어질 때, my_str을 길이 n씩 잘라서 저장한 배열을 return하도록 solution 함수를 완성해주세요.
+
+ 입출력 예
+ my_str    n    result
+ "abc1Addfggg4556b"    6    ["abc1Ad", "dfggg4", "556b"]
+ "abcdef123"    3    ["abc", "def", "123"]
+ */
+
+func solution_n_cut_array(_ my_str:String, _ n:Int) -> [String] {
+      var arr = [String]()
+      var str = ""
+      
+      for i in my_str.enumerated() {
+
+          if i.offset % n == 0 && i.offset != 0{
+              arr.append(str)
+              str = ""
+          }
+          str += String(i.element)
+          
+          if my_str.count - 1 == i.offset {
+              arr.append(str)
+          }
+
+      }
+      return arr
+}
+
+print(solution_n_cut_array("abc1Addfggg4556b", 6))
