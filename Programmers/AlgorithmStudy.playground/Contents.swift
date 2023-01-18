18fe3772d533b868cfec69d8827096efe0af5779
JIINHEO <gjwldls123@gmail.com>
2023-01-19 01:30:16 +0900

    Lv0. 다항식 더하기

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -418,3 +418,53 @@ func solution_cha_coordinate(_ keyinput:[String], _ board:[Int]) -> [Int] {
 print(solution_cha_coordinate( ["left", "right", "up", "right", "right"], [11, 11]))
 print(solution_cha_coordinate( ["down", "down", "down", "down", "down"], [7, 9]))
 print("=================================================")
+
+
+/*
+ [ 다항식 더하기 ]
+ 
+ 한 개 이상의 항의 합으로 이루어진 식을 다항식이라고 합니다. 다항식을 계산할 때는 동류항끼리 계산해 정리합니다. 덧셈으로 이루어진 다항식 polynomial이 매개변수로 주어질 때, 동류항끼리 더한 결괏값을 문자열로 return 하도록 solution 함수를 완성해보세요. 같은 식이라면 가장 짧은 수식을 return 합니다.
+ 
+ 입출력 예
+ polynomial    result
+ "3x + 7 + x"    "4x + 7"
+ "x + x + x"    "3x"
+ https://school.programmers.co.kr/learn/courses/30/lessons/120863
+ */
+
+func solution_polynomial_add(_ polynomial:String) -> String {
+    var x = 0
+    var num = 0
+    
+    let arr = polynomial.split(separator: " ")
+    
+    for i in arr where !i.contains("+"){
+        if i.contains("x"){
+            if i == "x" {
+                x += 1
+            } else {
+                let strX = i.replacingOccurrences(of: "x", with: "")
+                x += Int(strX)!
+            }
+        } else {
+            num += Int(i)!
+        }
+    }
+    
+    if x > 0 && num > 0{
+        if x == 1 {
+            return   "x + \(num)"
+        }
+        return  "\(x)x + \(num)"
+    } else if x == 0 && num > 0{
+        return   "\(num)"
+    } else if x == 1 {
+        return "x"
+    }
+    return "\(x)x"
+}
+
+print(solution_polynomial_add("3x + 7 + x"))
+print(solution_polynomial_add("x + 7 + 1"))
+print(solution_polynomial_add("1 + 7 + 1"))
+print("=================================================")
