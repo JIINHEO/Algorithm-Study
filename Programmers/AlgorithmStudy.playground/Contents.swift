3bcd7a3ab46c5a03699151b6bc40183fb851279d
JIINHEO <gjwldls123@gmail.com>
2023-01-20 00:10:53 +0900

    Lv0. 외계어 사전

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -499,3 +499,44 @@ func solution_babbling(_ babbling:[String]) -> Int {
 print(solution_babbling(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
 print("=================================================")
 
+/*
+ [ 외계어 사전 ]
+ 
+ PROGRAMMERS-962 행성에 불시착한 우주비행사 머쓱이는 외계행성의 언어를 공부하려고 합니다. 알파벳이 담긴 배열 spell과 외계어 사전 dic이 매개변수로 주어집니다. spell에 담긴 알파벳을 한번씩만 모두 사용한 단어가 dic에 존재한다면 1, 존재하지 않는다면 2를 return하도록 solution 함수를 완성해주세요.
+ 
+ 입출력 예
+ spell    dic    result
+ ["p", "o", "s"]    ["sod", "eocd", "qixm", "adio", "soo"]    2
+ ["z", "d", "x"]    ["def", "dww", "dzx", "loveaw"]    1
+ ["s", "o", "m", "d"]    ["moos", "dzx", "smm", "sunmmo", "som"]    2
+ */
+
+func solution_spell_dic(_ spell:[String], _ dic:[String]) -> Int {
+    var arr = [String]()
+    for item in dic {
+        if item.count == spell.count {
+            arr.append(item)
+        }
+    }
+    for item in arr {
+        for j in spell.enumerated() {
+            let count = item.components(separatedBy: j.element).count
+            if  count != 2 {
+                break
+            }
+            if j.offset == spell.count - 1{
+                return 1
+            }
+        }
+    }
+    return 2
+    
+  /*
+    func solution(_ spell: [String], _ dic: [String]) -> Int {
+    dic.map { String($0.sorted()) }.contains(spell.sorted().joined()) ? 1 : 2 }
+   */
+}
+
+print(solution_spell_dic( ["z", "d", "x"], ["def", "dww", "dzx", "loveaw"]))
+print(solution_spell_dic( ["s", "o", "m", "d"], ["moos", "dzx", "smm", "sunmmo", "som"]))
+print("=================================================")
