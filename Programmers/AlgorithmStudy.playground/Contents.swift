2a6544bfeec21c1cdbbbb6637908de8199442ab3
JIINHEO <gjwldls123@gmail.com>
2023-01-19 23:30:54 +0900

    Lv0. 옹알이(1)

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -468,3 +468,34 @@ print(solution_polynomial_add("3x + 7 + x"))
 print(solution_polynomial_add("x + 7 + 1"))
 print(solution_polynomial_add("1 + 7 + 1"))
 print("=================================================")
+
+/*
+ [ 옹알이 (1) ]
+ 
+ 머쓱이는 태어난 지 6개월 된 조카를 돌보고 있습니다. 조카는 아직 "aya", "ye", "woo", "ma" 네 가지 발음을 최대 한 번씩 사용해 조합한(이어 붙인) 발음밖에 하지 못합니다. 문자열 배열 babbling이 매개변수로 주어질 때, 머쓱이의 조카가 발음할 수 있는 단어의 개수를 return하도록 solution 함수를 완성해주세요.
+ 
+ 입출력 예
+ babbling    result
+ ["aya", "yee", "u", "maa", "wyeoo"]    1
+ ["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]    3
+ */
+
+func solution_babbling(_ babbling:[String]) -> Int {
+    var result = 0
+    for items in babbling {
+        var item = items
+        item = item.replacingOccurrences(of: "aya", with: "-")
+        item = item.replacingOccurrences(of: "ye", with: "-")
+        item = item.replacingOccurrences(of: "woo", with: "-")
+        item = item.replacingOccurrences(of: "ma", with: "-")
+        item = item.replacingOccurrences(of: "-", with: "")
+        if item == "" {
+            result += 1
+        }
+    }
+    return result
+}
+
+print(solution_babbling(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
+print("=================================================")
+
