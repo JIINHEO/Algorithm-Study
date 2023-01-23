3f4352d77567c564474fbe9f50c9136e303b3054
jiinheo <gjwldls123@gmail.com>
2023-01-23 18:15:06 +0900

    Lv0. 삼각형의 완성조건 (2)

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -241,3 +241,51 @@ func solution_en_to_num(_ numbers:String) -> Int64 {
 print(solution_en_to_num("onetwothreefourfivesixseveneightnine"))
 print(solution_en_to_num("onefourzerosixseven"))
 print("=================================================")
+
+/*
+ [ 삼각혁의 완성조건 (2) ]
+ 
+ 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+
+ 가장 긴 변의 길이는 다른 두 변의 길이의 합보다 작아야 합니다.
+ 삼각형의 두 변의 길이가 담긴 배열 sides이 매개변수로 주어집니다. 나머지 한 변이 될 수 있는 정수의 개수를 return하도록 solution 함수를 완성해주세요.
+
+ 선분 세 개로 삼각형을 만들기 위해서는 다음과 같은 조건을 만족해야 합니다.
+
+ 입출력 예
+ sides    result
+ [1, 2]    1
+ [3, 6]    5
+ [11, 7]    13
+ https://school.programmers.co.kr/learn/courses/30/lessons/120868
+ */
+
+func solution_triangle2(_ sides:[Int]) -> Int {
+    let a = sides[0]
+    let b = sides[1]
+    let sum = a + b
+
+    var max = a < b ? b : a
+    var min = a < b ? a : b
+    
+    var result = 0
+    // case1. 한 변이 sides에 있을 때
+    for i in 0...sum {
+        if max < min + i && min + i < sum {
+            result += 1
+        }
+    }
+
+    // case2. 한 변이 나머지 한 변일 때
+    for _ in max..<sum {
+        result += 1
+    }
+
+    return result
+}
+
+
+print(solution_triangle2([1, 2]))
+print(solution_triangle2([3, 6]))
+print(solution_triangle2([11, 7]))
+print("=================================================")
