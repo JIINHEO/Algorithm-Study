30a07215fbbcc923a4ee4a76b5caa8710ecc9251
jiinheo <gjwldls123@gmail.com>
2023-01-23 18:46:44 +0900

    Lv0. 로그인 성공?

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -615,3 +615,47 @@ print(solution_cut_paper(2, 2))
 print(solution_cut_paper(2, 5))
 print(solution_cut_paper(1, 1))
 print("=================================================")
+
+/*
+ [ 로그인 성공? ]
+ 
+ 머쓱이는 프로그래머스에 로그인하려고 합니다. 머쓱이가 입력한 아이디와 패스워드가 담긴 배열 id_pw와 회원들의 정보가 담긴 2차원 배열 db가 주어질 때, 다음과 같이 로그인 성공, 실패에 따른 메시지를 return하도록 solution 함수를 완성해주세요.
+
+ 아이디와 비밀번호가 모두 일치하는 회원정보가 있으면 "login"을 return합니다.
+ 로그인이 실패했을 때 아이디가 일치하는 회원이 없다면 “fail”를, 아이디는 일치하지만 비밀번호가 일치하는 회원이 없다면 “wrong pw”를 return 합니다.
+ 
+ 회원들의 아이디는 같을 수 있지만 아이디는 같을 수 없다
+
+ 입출력 예
+ id_pw    db    result
+ ["meosseugi", "1234"]    [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]    "login"
+ ["programmer01", "15789"]    [["programmer02", "111111"], ["programmer00", "134"], ["programmer01", "1145"]]    "wrong pw"
+ ["rabbit04", "98761"]    [["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]    "fail"
+ https://school.programmers.co.kr/learn/courses/30/lessons/120883
+ */
+
+func solution_login_true_or_false(_ id_pw:[String], _ db:[[String]]) -> String {
+    let id = id_pw[0]
+    let pw = id_pw[1]
+    var arrID = [[String]]()
+    
+    for item in db.enumerated() {
+        if id == item.element[0]{
+            arrID.append(item.element)
+            if pw == item.element[1] {
+                return "login"
+            }
+        }
+    }
+    
+    if arrID.count > 0 {
+        return "wrong pw"
+    } else {
+        return "fail"
+    }
+}
+
+print(solution_login_true_or_false(["meosseugi", "1234"], [["rardss", "123"], ["yyoom", "1234"], ["meosseugi", "1234"]]))
+print(solution_login_true_or_false(["rabbit04", "98761"], [["jaja11", "98761"], ["krong0313", "29440"], ["rabbit00", "111333"]]))
+
+
