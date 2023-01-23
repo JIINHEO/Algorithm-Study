7b7fb810aedcab61a7d889103b710411eeecc87b
jiinheo <gjwldls123@gmail.com>
2023-01-23 20:27:12 +0900

    Lv0. 치킨 쿠폰

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -695,3 +695,30 @@ print(solution_push_string("atat", "tata"))
 print(solution_push_string("abc", "abc"))
 print(solution_push_string("asgwrfer2", "fer2asgwr"))
 print("=================================================")
+
+/*
+ [ 치킨 쿠폰 ]
+ 
+ 프로그래머스 치킨은 치킨을 시켜먹으면 한 마리당 쿠폰을 한 장 발급합니다. 쿠폰을 열 장 모으면 치킨을 한 마리 서비스로 받을 수 있고, 서비스 치킨에도 쿠폰이 발급됩니다. 시켜먹은 치킨의 수 chicken이 매개변수로 주어질 때 받을 수 있는 최대 서비스 치킨의 수를 return하도록 solution 함수를 완성해주세요.
+ 
+ 입출력 예
+ chicken    result
+ 100    11
+ 1,081    120
+ https://school.programmers.co.kr/learn/courses/30/lessons/120884
+ */
+
+func solution_service_chicken(_ chicken:Int) -> Int {
+    var cupon = chicken
+    var serviceChicken = 0
+    while cupon / 10 > 0 {
+        serviceChicken += cupon / 10
+        cupon = (cupon / 10) + (cupon % 10)
+    }
+    
+    return serviceChicken
+}
+
+print(solution_service_chicken(100))
+print(solution_service_chicken(1081))
+print("=================================================")
