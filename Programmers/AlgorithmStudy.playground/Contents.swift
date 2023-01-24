1006e1e05936281a20de5ed333d3130829b58cb2
jiinheo <gjwldls123@gmail.com>
2023-01-24 22:56:21 +0900

    Lv0. 겹치는 선분의 길이

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -957,3 +957,46 @@ print(solution_sequence_num_sum(5, 15)) // 1, 2, 3, 4, 5
 print(solution_sequence_num_sum(4, 14)) // 2, 3, 4, 5
 print(solution_sequence_num_sum(5, 5)) // -1, 0, 1, 2, 3
 print("=================================================")
+
+/*
+ [ 겹치는 선분의 길이 ]
+ 선분 3개가 평행하게 놓여 있습니다. 세 선분의 시작과 끝 좌표가 [[start, end], [start, end], [start, end]] 형태로 들어있는 2차원 배열 lines가 매개변수로 주어질 때, 두 개 이상의 선분이 겹치는 부분의 길이를 return 하도록 solution 함수를 완성해보세요.
+
+ lines가 [[0, 2], [-3, -1], [-2, 1]]일 때 그림으로 나타내면 다음과 같습니다.
+ 선분이 두 개 이상 겹친 곳은 [-2, -1], [0, 1]로 길이 2만큼 겹쳐있습니다.
+ 
+ 입출력 예
+ lines    result
+ [[0, 1], [2, 5], [3, 9]]    2
+ [[-1, 1], [1, 3], [3, 9]]    0
+ [[0, 5], [3, 9], [1, 10]]    8
+ https://school.programmers.co.kr/learn/courses/30/lessons/120876
+ */
+
+func solution_overlab_line(_ lines:[[Int]]) -> Int {
+    
+    let min = lines.flatMap{ $0 }.min()!
+    let max = lines.flatMap{ $0 }.max()!
+    
+    // dic를 min부터 max까지 0으로 만들고
+    // 각 키의 vlaue를 +1로 update 하자.. 면 안되네 흐
+    var dic = [Int: Int]()
+    
+    for i in min...max {
+        dic[i] = 0
+    }
+    
+    for item in lines {
+        for j in item[0]..<item[1] {
+            let count = dic[j]!
+            dic[j] = count + 1
+        }
+    }
+
+    return dic.filter{$0.value > 1}.count
+}
+
+print(solution_overlab_line([[0, 1], [2, 5], [3, 9]]))
+print(solution_overlab_line([[-1, 1], [1, 3], [3, 9]]))
+print(solution_overlab_line([[0, 5], [3, 9], [1, 10]]))
+print("=================================================")
