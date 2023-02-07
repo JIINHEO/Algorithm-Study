fa3af3ebb2799c64ffc517a787db5a6efb34f277
JIINHEO <gjwldls123@gmail.com>
2023-02-07 21:46:56 +0900

    스택/큐.올바른 괄호

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -1115,3 +1115,50 @@ print(solution_safe_area( [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0
 print(solution_safe_area( [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]] ))
 print("=================================================")
 
+/*
+ [ 올바른 괄호 ] 스택/큐
+ 문제 설명
+ 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 예를 들어
+
+ "()()" 또는 "(())()" 는 올바른 괄호입니다.
+ ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+ '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+ 입출력 예
+ s    answer
+ "()()"    true
+ "(())()"    true
+ ")()("    false
+ "(()("    false
+ 
+ https://school.programmers.co.kr/learn/courses/30/lessons/12909
+ */
+
+func solution_braket(_ s:String) -> Bool
+{
+    let arr = Array(s)
+    var newArr = [Character]()
+    
+    if arr.first == ")" {
+        return false
+    }
+    
+    for i in arr {
+        if i == "(" {
+            newArr.append(i)
+        } else {
+            if newArr.count == 0 {
+                return false
+            }
+            newArr.removeLast()
+        }
+    }
+
+    return  newArr.count == 0 ? true : false
+}
+
+print(solution_braket("()()"))
+print(solution_braket("(())()"))
+print(solution_braket(")()("))
+print("=================================================")
+
