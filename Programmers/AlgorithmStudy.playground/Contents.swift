a95bdf21f8fb5992de2f392e3a7956af5d0487aa
JIINHEO <gjwldls123@gmail.com>
2023-02-09 23:04:03 +0900

    Lv2. 가장 큰 수

--- Programmers/AlgorithmStudy.playground/Contents.swift
+++ Programmers/AlgorithmStudy.playground/Contents.swift
@@ -1162,3 +1162,36 @@ print(solution_braket("(())()"))
 print(solution_braket(")()("))
 print("=================================================")
 
+/*
+ [ 가장 큰 수 ]
+ 
+ 0 또는 양의 정수가 주어졌을 때, 정수를 이어 붙여 만들 수 있는 가장 큰 수를 알아내 주세요.
+
+ 예를 들어, 주어진 정수가 [6, 10, 2]라면 [6102, 6210, 1062, 1026, 2610, 2106]를 만들 수 있고, 이중 가장 큰 수는 6210입니다.
+
+ 0 또는 양의 정수가 담긴 배열 numbers가 매개변수로 주어질 때, 순서를 재배치하여 만들 수 있는 가장 큰 수를 문자열로 바꾸어 return 하도록 solution 함수를 작성해주세요.
+ 
+ 입출력 예
+ numbers    return
+ [6, 10, 2]    "6210"
+ [3, 30, 34, 5, 9]    "9534330"
+ 
+ https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=swift
+ */
+
+func solution_max_num(_ numbers:[Int]) -> String {
+    
+    if numbers.sorted().last == 0 {
+        return "0"
+    } else {
+        let arrSort = numbers.sorted { $0 > $1 }.map{ String($0) }
+        let arr = arrSort.sorted{ Int("\($0)\($1)")! > Int("\($1)\($0)")! }
+        return arr.joined()
+    }
+}
+
+print(solution_max_num([6, 10, 2]))
+print(solution_max_num([3, 30, 34, 5, 9]))
+print(solution_max_num([1, 10, 100, 1000, 818, 81, 898, 89, 0, 0]))
+print(solution_max_num([12, 1213]))
+print("=================================================")
