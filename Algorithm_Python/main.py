ab675badfd9361efa60d4cc7140b91914da57c87
jiinheo <gjwldls123@gmail.com>
2023-04-09 23:58:53 +0900

    Add python project

---
+++ Algorithm_Python/main.py
@@ -0,0 +1,16 @@
+import Programmers.lv1_내적
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+
+
+print(Programmers.lv1_내적.solution([1,2,3,4], [-3,-1,0,2]))
+
+# print(solution_dot_product([1,2,3,4], [-3,-1,0,2]))
