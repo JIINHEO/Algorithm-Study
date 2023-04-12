b2cc78976ca537b6b8ef2be67e06c0ea8d5bd703
JIINHEO <gjwldls123@gmail.com>
2023-04-12 23:46:50 +0900

    Add solution

--- Algorithm_Python/main.py
+++ Algorithm_Python/main.py
@@ -8,16 +8,28 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # import Programmers.lv1_약수의개수와덧셈
-#
 # print(Programmers.lv1_약수의개수와덧셈.solution(13, 17))
 
 # import Programmers.lv1_3진법뒤집기
-#
 # print(Programmers.lv1_3진법뒤집기.solution(45))
 
 # import Programmers.lv1_크기가작은부분문자열
-#
 # print(Programmers.lv1_크기가작은부분문자열.solution('3141592', '271'))
 
-import Programmers.lv1_두개뽑아서더하기
-print(Programmers.lv1_두개뽑아서더하기.solution([2, 1, 3, 4, 1]))
+# import Programmers.lv1_두개뽑아서더하기
+# print(Programmers.lv1_두개뽑아서더하기.solution([2, 1, 3, 4, 1]))
+
+# import Programmers.lv1_삼총사
+# print(Programmers.lv1_삼총사.solution([-2, 3, 0, 2, -5]))
+
+# import  Programmers.lv1_최소직사각형
+# print(Programmers.lv1_최소직사각형.solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+
+# import  Programmers.lv1_1차비밀지도
+# print(Programmers.lv1_1차비밀지도.solution(6, [46, 33, 33 ,22, 31, 50], [27 ,56, 19, 14, 14, 10]))
+
+# import Programmers.lv1_숫자문자열과영단어
+# print(Programmers.lv1_숫자문자열과영단어.solution("2three45sixseven"))
+
+import Programmers.lv1_소수찾기
+print(Programmers.lv1_소수찾기.solution(1000000))
